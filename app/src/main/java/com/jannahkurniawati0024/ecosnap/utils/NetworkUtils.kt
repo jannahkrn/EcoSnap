289f42f052525de41d4547392b92c80fd598694b
Jannah Kurniawati <jannahkurnia15@gmail.com>
2026-06-23 00:11:45 +0700

    File baru, utilitas cek koneksi internet via ConnectivityManager

---
+++ app/src/main/java/com/jannahkurniawati0024/ecosnap/utils/NetworkUtils.kt
@@ -0,0 +1,16 @@
+package com.jannahkurniawati0024.ecosnap.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+    fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(network) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
